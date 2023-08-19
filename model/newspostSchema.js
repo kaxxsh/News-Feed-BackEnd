3059603eeaed2bcee1e035ff6c93a6fb520d5e0d
kaxxsh <kameshwaran2002@gmail.com>
2023-08-19 23:56:56 +0530

    newspost schema update

---
+++ model/newspostSchema.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+
+const Schema = mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+    image: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const newspost = mongoose.model("newspost", Schema);
+
+export default newspost;
